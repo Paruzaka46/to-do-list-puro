e4704b25bb08314f1c47257bac1cdbd7169259c3
Rayhan Fallah <rayhanfallah29@gmail.com>
2023-09-28 00:20:02 +0700

    Patch 02

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
-const todoArr = [];
 const todoArrWork = [];
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,24 +12,26 @@ mongoose.connect("mongodb://127.0.0.1:27017/todoListDB");
 const itemsSchema = new mongoose.Schema({
   name: String,
 });
-const Item = mongoose.model("Item", itemsSchema);
 
-const item1 = new Item({
+const Item = mongoose.model("Item", itemsSchema);
+const Work = mongoose.model("Work", itemsSchema);
+
+const item1 = new Work({
   name: "Do coding",
 });
 
-const item2 = new Item({
+const item2 = new Work({
   name: "Eat",
 });
 
-const item3 = new Item({
+const item3 = new Work({
   name: "Sleep",
 });
 
 const arr = [item1, item2, item3];
 
 // try {
-//   Item.insertMany(arr);
+//   Work.insertMany(arr);
 //   console.log("Insert succesfully");
 // } catch (error) {
 //   console.log(error);
@@ -52,27 +53,77 @@ app.get("/", (req, res) => {
 });
 
 app.get("/work", (req, res) => {
-  res.render("work.ejs", {
-    addTodoWork: todoArrWork,
-  });
+  Work.find({})
+    .then(function (foundItems) {
+      console.log(foundItems);
+      res.render("work.ejs", {
+        addTodo: foundItems,
+      });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
 });
+// res.render("work.ejs", {
+//   addTodoWork: todoArrWork,
+// });;
 
 app.post("/", (req, res) => {
-  todoArr.push(req.body["new"]);
-  console.log(todoArr);
-  res.render("index.ejs", {
-    addTodo: todoArr,
-    today: day[new Date().getDay()],
-    month: month[new Date().getMonth()],
+  // todoArr.push(req.body["new"]);
+  const userInput = req.body.new;
+  console.log(userInput);
+
+  const savetoDB = new Item({
+    name: userInput,
   });
+
+  savetoDB.save();
+
+  res.redirect("/");
+
+  // res.render("index.ejs", {
+  //   addTodo: foundItems,
+  //   today: day[new Date().getDay()],
+  //   month: month[new Date().getMonth()],
+  // });
+});
+
+app.post("/delete", (req, res) => {
+  console.log(req.body.checkbox);
+  Item.deleteOne({ _id: req.body.checkbox })
+    .then(() => {
+      console.log("Delete Succesfully");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/");
 });
 
 app.post("/work", (req, res) => {
-  todoArrWork.push(req.body["new"]);
-  console.log(todoArrWork);
-  res.render("work.ejs", {
-    addTodoWork: todoArrWork,
+  const workTodo = req.body.new;
+  const savetoDB = new Work({
+    name: workTodo,
   });
+  savetoDB.save();
+  res.redirect("/work");
+  // todoArrWork.push(req.body["new"]);
+  // console.log(todoArrWork);
+  // res.render("work.ejs", {
+  //   addTodoWork: todoArrWork,
+  // });
+});
+
+app.post("/deletework", (req, res) => {
+  console.log(req.body.checkbox);
+  Work.deleteOne({ _id: req.body.checkbox })
+    .then(() => {
+      console.log("Delete Succesfully");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/work");
 });
 
 app.listen(port, () => {
